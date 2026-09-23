92d0d8345df2464ea5fcab9e3c59646776382eb3
lavendercloudwalker <monyetmixmouse@gmail.com>
2026-09-23 21:21:50 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this number whenever you upload a new version of the app
-const VERSION = 'v9';
+const VERSION = 'v10';
 const CACHE = 'glute-tracker-' + VERSION;
 const CORE = ['./', './index.html', './manifest.webmanifest',
   './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
